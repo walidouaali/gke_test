290695b068768d9cdfc52148ed80d5e1f6f781b2
ouaali <walid.ouaali@imt-atlantique.fr>
2025-12-18 00:20:06 +0100

    fixing issue

--- app1/app.py
+++ app1/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 # Create a Flask application
 app = Flask(__name__)
 
-# Define a route for the root URL
+# Define a  route for the root URL
 @app.route('/')
 def hello_world():
     return 'Hello, World App 1!'
